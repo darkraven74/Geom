3950903e063ab33019b1acfe8fa635cb97dcfde5
Vladimir Baev <darkraven8@gmail.com>
2013-04-20 06:46:14 +0400

    practice4 added

--- Practice3/Proj/main.cpp
+++ Practice3/Proj/main.cpp
@@ -59,84 +59,84 @@ TEST(test_intersect, 1)
 {
     segment_2t<double> s1(point_2t<double>(1, 0), point_2t<double>(5, 0));
     segment_2t<double> s2(point_2t<double>(10, 10), point_2t<double>(10, -10));
-    EXPECT_EQ(false, intersect(s1, s2));
+    EXPECT_FALSE(intersect(s1, s2));
 }
 
 TEST(test_intersect, 2)
 {
     segment_2t<double> s1(point_2t<double>(1, 0), point_2t<double>(5, 0));
     segment_2t<double> s2(point_2t<double>(3, 10), point_2t<double>(3, -10));
-    EXPECT_EQ(true, intersect(s1, s2));
+    EXPECT_TRUE(intersect(s1, s2));
 }
 
 TEST(test_intersect, 3)
 {
     segment_2t<double> s1(point_2t<double>(3, 3), point_2t<double>(1, 1));
     segment_2t<double> s2(point_2t<double>(5, 5), point_2t<double>(4, 4));
-    EXPECT_EQ(false, intersect(s1, s2));
+    EXPECT_FALSE(intersect(s1, s2));
 }
 
 TEST(test_intersect, 4)
 {
     segment_2t<double> s1(point_2t<double>(3, 3), point_2t<double>(1, 1));
     segment_2t<double> s2(point_2t<double>(2, 2), point_2t<double>(4, 4));
-   EXPECT_EQ(true, intersect(s1, s2));
+   EXPECT_TRUE(intersect(s1, s2));
 }
 
 TEST(test_intersect, 5)
 {
     segment_2t<double> s1(point_2t<double>(1, 1), point_2t<double>(5, 5));
     segment_2t<double> s2(point_2t<double>(4, 4), point_2t<double>(3, -7));
-    EXPECT_EQ(true, intersect(s1, s2));
+    EXPECT_TRUE(intersect(s1, s2));
 }
 
 TEST(test_intersect, 6)
 {
     segment_2t<double> s1(point_2t<double>(1, 0), point_2t<double>(5, 0));
     segment_2t<double> s2(point_2t<double>(3, 7), point_2t<double>(3, 10));
-    EXPECT_EQ(false, intersect(s1, s2));
+    EXPECT_FALSE(intersect(s1, s2));
 }
 
 TEST(test_intersect, 7)
 {
     segment_2t<double> s1(point_2t<double>(1, 0), point_2t<double>(5, 0));
     segment_2t<double> s2(point_2t<double>(7, 7), point_2t<double>(10, 10));
-    EXPECT_EQ(false, intersect(s1, s2));
+    EXPECT_FALSE(intersect(s1, s2));
 }
 
 TEST(test_intersect, 8)
 {
     segment_2t<double> s1(point_2t<double>(1, 0), point_2t<double>(5, 0));
     segment_2t<double> s2(point_2t<double>(5, 0), point_2t<double>(5, 10));
-    EXPECT_EQ(true, intersect(s1, s2));
+    EXPECT_TRUE(intersect(s1, s2));
 }
 
 TEST(test_intersect, 9)
 {
     segment_2t<double> s1(point_2t<double>(1, 1), point_2t<double>(5, 5));
     segment_2t<double> s2(point_2t<double>(7, 7), point_2t<double>(7, 7));
-    EXPECT_EQ(false, intersect(s1, s2));
+    EXPECT_FALSE(intersect(s1, s2));
 }
 
 TEST(test_intersect, 10)
 {
     segment_2t<double> s1(point_2t<double>(1, 1), point_2t<double>(5, 5));
     segment_2t<double> s2(point_2t<double>(4, 4), point_2t<double>(4, 4));
-    EXPECT_EQ(true, intersect(s1, s2));
+    EXPECT_TRUE(intersect(s1, s2));
 }
 
 TEST(test_intersect, 11)
 {
     segment_2t<double> s1(point_2t<double>(1, 1), point_2t<double>(1, 1));
     segment_2t<double> s2(point_2t<double>(1, 1), point_2t<double>(1, 1));
-    EXPECT_EQ(true, intersect(s1, s2));
+    EXPECT_TRUE(intersect(s1, s2));
 }
 
 TEST(test_intersect, 12)
 {
     segment_2t<double> s1(point_2t<double>(1, 1), point_2t<double>(3, 3));
     segment_2t<double> s2(point_2t<double>(100, 100), point_2t<double>(100, 1));
-    EXPECT_EQ(false, intersect(s1, s2));
+    EXPECT_FALSE(intersect(s1, s2));
 }
 
 int main(int argc, char ** argv)
