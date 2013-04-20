a471d8a20f808d293812c155713dd77cd42b508c
Vladimir Baev <darkraven8@gmail.com>
2013-04-20 06:47:22 +0400

    practice4 added

---
+++ Practice4/Proj/main.cpp
@@ -0,0 +1,19 @@
+#include <gtest/gtest.h>
+#include <iostream>
+#include <gmpxx.h>
+#include "cg/primitives/point.h"
+#include "cg/primitives/range.h"
+#include "cg/primitives/segment.h"
+#include <cg/operations/orientation.h>
+using namespace cg;
+using namespace std;
+
+
+int main(int argc, char ** argv)
+{
+
+
+
+   return 0;
+}
+
